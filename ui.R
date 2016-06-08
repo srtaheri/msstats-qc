a6579702891e468824dd207e710b931f65408cd6
Eralp DOGU <eralp.dogu@gmail.com>
2016-06-08 18:32:44 -0400

    ui updated

--- ui.R
+++ ui.R
@@ -8,17 +8,18 @@ shinyUI(fluidPage(
                          tags$img(src='logo.png', height=220, width=220, style = "float: right"),
                          p("MSstatsQC is an open-source web-based software which provides longitudinal
                            system suitability monitoring tools (control charts) for SRM based proteomic experiments."),
-                        h5("Statistical functionalities"),
+                        h5(strong("Metrics you can monitor")),
                         p("MSstatsQC uses control charts to monitor the instrument performance by tracking system
                            suitability metrics including total peak area, retention time and full width at half maximum (FWHM) and peak assymetry."),
-                         p("This framework includes simultaneous monitoring tools for mean and dispersion of suitability metrics and presents
+                       h5(strong("Statistical functionalities")),
+                        p("This framework includes simultaneous monitoring tools for mean and dispersion of suitability metrics and presents
                            alternative methods of monitoring such as time weighted control charts to ensure that various types
                            of process disturbances are detected effectively. Simultaneous control charts used in this framework
                            can be classified into two groups: individual-moving range (XmR) control charts and mean and dispersion
                            cumulative sum (CUSUM) control charts. To successfully identify the time of change, change point analysis
                            is also included in this framework. Experiment specific control limits are provided with the control 
                            charts to distinguish between random noise and systematic error."),
-                       h5("Using MSstatsQC"),
+                       h5(strong("Using MSstatsQC")),
                        p("The steps for generating results are as follows:"),
                          ("1. Import your QC data "),
                          br(),
@@ -632,17 +633,17 @@ shinyUI(fluidPage(
                                     the corresponding solute peak. The retention time is a measure of the amount of time a solute spends in a column. 
                                     It is the sum of the time spent in the stationary phase and the mobile phase."
                                     ,a("visit for more info",href="http://www.britannica.com/science/retention-time")),
-                                  
+                                  br(),
                                   h5(strong("Total Peak Area")),
                                   p("Total Peak Area is the sum of all integrated signals for a certain peptide."),
-                                  
+                                  br(),
                                   h5(strong("Full Width at Half Maximum (FWHM)")),
                                   p("Full width at half maximum 'FWHM' is an expression of the extent of a 
                                     function given by the difference between the two extreme values 
                                     of the independent variable at which the dependent variable is equal 
                                     to half of its maximum value." 
                                     ,a("visit for more info",href="https://en.wikipedia.org/wiki/Full_width_at_half_maximum")),
-                                  
+                                  br(),
                                   h5(strong("Peak Assymetry")),
                                   p("Peak Assymetry is a measure of symetry for a peak. Calculated by taking 2*a/(a+b). Optimal value is around 1 for a Gaussian peak.")
                                   ),
@@ -650,48 +651,48 @@ shinyUI(fluidPage(
                          tabPanel("Plots"
                                   
                                   ,h5(strong("XmR control charts")),
-                                  h5("Can detect=Large shifts and spikes in mean and dispersion of suitability metric."),
-                                  h5("Statistics used to construct=using the sequential differences between successive values as a measure 
-                                    of dispersion."),
-                                  p("When data are collected as individual observations, you cannot calculate the standard 
-                                    deviation for each subgroup. The moving range is an alternative way to calculate process 
-                                    variation by computing the ranges of two or more consecutive observations.XmR is a cobination 
-                                    of a chart for individual observations and 
-                                    a chart for moving ranges. I-MR chart consists of 2 charts; 
+                                  h5("Can detect large shifts and spikes in the mean and dispersion of suitability metric."),
+                                  h5("The sequential differences between successive values as a measure 
+                                    of dispersion and individual observations are used to construct the plots."),
+                                  p("A measure of dispersion can be estimated by computing the ranges of two consecutive observations. This 
+                                  approach is used to construct XmR charts. XmR charts plot 
+                                    original observations and moving ranges to investigate deviations from random process behaviour.
+                                    XmR chart consists of 2 charts; 
                                     Individuals (X) chart and Moving Range (mR) chart. 
                                     ", 
-                                    a("visit for more info",href="https://en.wikipedia.org/wiki/Shewhart_individuals_control_chart"))
+                                  a("visit for more info",href="https://en.wikipedia.org/wiki/Shewhart_individuals_control_chart")),
                                   
-                                  
-                                  ,h5(strong("CUSUMm and CUSUMv control charts")),
-                                  h5("Can detect=Large shifts and spikes in mean and dispersion of suitability metric."),
-                                  h5("Statistics used to construct=using the sequential differences between successive values as a measure 
-                                     of dispersion."),
+                                  br(),
+                                  h5(strong("CUSUMm and CUSUMv control charts")),
+                                  h5("Can detect small shifts and sustained drifts in the mean and dispersion of suitability metric."),
+                                  h5("Time weighted cumulative sums are used to construct the plots."),
                                     p("A CUSUM chart is a time-weighted control chart that displays the cumulative sums 
-                                    'CUSUMs' of the deviations of each sample value from the target value. Because it is cumulative, 
-                                    even minor drifting in the process mean will lead to steadily 
-                                    increasing or decreasing cumulative deviation values. In that case, you are often primarily interested 
-                                    in detecting small shifts in the product quality (for example, gradual deterioration of quality due to 
-                                    machine wear)", 
-                                    a("visit for more info",href="https://en.wikipedia.org/wiki/CUSUM"))
-                                
-                                  
-                                  ,h5(strong("Change Point Analysis")),
-                                  h5("Can detect=Large shifts and spikes in mean and dispersion of suitability metric."),
-                                  h5("Statistics used to construct=using the sequential differences between successive values as a measure 
-                                     of dispersion."),
+                                    'CUSUMs'. Because it is cumulative, even minor drifts in the process mean or dispersion and gradual deterioration of quality will cause steadily 
+                                    increasing or decreasing cumulative values. We introduce two CUSUM control charts: a mean CUSUM (CUSUMm) and 
+                                      a dispersion CUSUM (CUSUMv).", 
+                                  a("visit for more info",href="https://en.wikipedia.org/wiki/CUSUM")),
+                               
+                                  br(),
+                                  h5(strong("Change Point Analysis")),
+                                  h5("Can identify the exact time of a change in the mean and dispersion of suitability metric. "),
+                                  h5("Likelihood functions are plotted and the QC sample which maximizes the functions is considered as a candidate change point."),
                                   p("A change in the process parameters triggers a control chart to generate an out of
                                     control signal. The QC sample at which the signal is issued is considered as the 
-                                    stopping time and after the signal search for an assignable cause is recommended."
-                                  , a("visit for more info",href="http://www.eng.fsu.edu/~pigna/pdf/"))
+                                    stopping time and after the signal search for an assignable cause is recommended. 
+                                    However, the signal does not always designate that the special cause actually occurred 
+                                    at that certain time. A remedy to this problem is to use follow-up change 
+                                    point analysis along with control charts. Change point estimation procedures have a potential 
+                                    to save time by narrowing the search window. We introduce 
+                                    two change point models: step shift change model for mean and step shift change model for variance.", 
+                                   a("visit for more info",href="http://www.eng.fsu.edu/~pigna/pdf/"))
                          ),
                          
                          tabPanel("Documentation",
                                   h5(strong("MSstatsQC webpage")),
                                   p("Source codes, related documents and user manual can be found via our MSstats website"
-                                    , a("visit for more info",href="http://www.msstats.org/msstatsqc"))
-                                  
-                                  ,h5(strong("MSstatsQC Github")),
+                                    , a("visit for more info",href="http://www.msstats.org/msstatsqc")),
+                                  br(),
+                                  h5(strong("MSstatsQC Github")),
                                   p("Latest documantation is also available via our Github page"
                                     , a("visit for more info",href="https://github.com/srtaheri/msstats-qc"))
       
